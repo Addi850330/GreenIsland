7e87afecc1a964b06e926c24478f9ce3196f322c
Shiuan <berserker850330@gmail.com>
2023-11-18 00:07:23 +0800

    hidenav 初步完成

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,130 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import style from "../app/page.module.css";
 const Nav = (props) => {
+  const [navhide, setNavhide] = useState("close");
+
+  const navswitch = function () {
+    if (navhide === "close") {
+      setNavhide("open");
+    } else {
+      setNavhide("close");
+    }
+  };
+
   return (
-    <nav className={style.nav}>
-      <div className={style.icon}>
-        <div className={style.iconimg}>
-          <img onClick={props.testpage} src="/icon/iconLight.png" alt="" />
+    <>
+      <nav className={style.nav}>
+        <div className={style.icon}>
+          <div className={style.iconimg}>
+            <img onClick={props.testpage} src="/icon/iconLight.png" alt="" />
+          </div>
+          <div className={style.text}>
+            <div>Hunter × Hunter</div>
+            <div>Greed Island</div>
+          </div>
         </div>
-        <div className={style.text}>
-          <div>Hunter × Hunter</div>
-          <div>Greed Island</div>
+        <div className={style.title}>Card Collection</div>
+        <div onClick={navswitch} className={style.contentclose}>
+          <span
+            className={
+              navhide === "open"
+                ? `${style.firstline} ${style.firstlinehide}`
+                : `${style.firstline}`
+            }
+          ></span>
+          <span
+            className={
+              navhide === "open"
+                ? `${style.secondline} ${style.secondlinehide}`
+                : `${style.secondline}`
+            }
+          ></span>
+          <span
+            className={
+              navhide === "open"
+                ? `${style.thirdline} ${style.thirdlinehide}`
+                : `${style.thirdline}`
+            }
+          ></span>
         </div>
-      </div>
-      <div className={style.title}>Card Collection</div>
-      <div className={style.table}>
-        <ul className={style.contents}>
-          <li className={`${style.content} `}>
+        <div className={style.table}>
+          <ul className={style.contents}>
+            <li className={`${style.content} `}>
+              <Link
+                onClick={props.specify}
+                className={
+                  props.page === "specifycard"
+                    ? `${style.link} ${style.contentarrow}`
+                    : `${style.link}`
+                }
+                href="/specifycard"
+                scroll={false}
+              >
+                指定口袋
+              </Link>
+            </li>
+            <li className={style.content}>
+              <Link
+                onClick={props.magic}
+                className={
+                  props.page === "magiccard"
+                    ? `${style.link} ${style.contentarrow}`
+                    : `${style.link}`
+                }
+                href="/magiccard"
+                scroll={false}
+              >
+                咒語卡
+              </Link>
+            </li>
+            <li className={style.content}>
+              <Link
+                onClick={props.freeslot}
+                className={
+                  props.page === "freeslot"
+                    ? `${style.link} ${style.contentarrow}`
+                    : `${style.link}`
+                }
+                href="/freeslot"
+                scroll={false}
+              >
+                自由口袋
+              </Link>
+            </li>
+            <li className={style.content}>
+              <Link
+                onClick={props.manager}
+                className={
+                  props.page === "managercard"
+                    ? `${style.link} ${style.contentarrow}`
+                    : `${style.link}`
+                }
+                href="/managercard"
+                scroll={false}
+              >
+                管理者
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <nav className={style.hidenav}>
+        <ul
+          className={
+            navhide === "close"
+              ? `${style.hidecontents} ${style.hidposition}`
+              : `${style.hidecontents}`
+          }
+        >
+          <li className={style.content}>
             <Link
               onClick={props.specify}
               className={
                 props.page === "specifycard"
-                  ? `${style.link} ${style.contentarrow}`
+                  ? `${style.link} ${style.nharrow}`
                   : `${style.link}`
               }
               href="/specifycard"
@@ -36,8 +138,8 @@ const Nav = (props) => {
               onClick={props.magic}
               className={
                 props.page === "magiccard"
-                  ? `${style.link} ${style.contentarrow}`
-                  : `${style.link}`
+                  ? `${style.link} ${style.nharrow}`
+                  : `${style.link} `
               }
               href="/magiccard"
               scroll={false}
@@ -50,8 +152,8 @@ const Nav = (props) => {
               onClick={props.freeslot}
               className={
                 props.page === "freeslot"
-                  ? `${style.link} ${style.contentarrow}`
-                  : `${style.link}`
+                  ? `${style.link} ${style.nharrow}`
+                  : `${style.link} `
               }
               href="/freeslot"
               scroll={false}
@@ -64,8 +166,8 @@ const Nav = (props) => {
               onClick={props.manager}
               className={
                 props.page === "managercard"
-                  ? `${style.link} ${style.contentarrow}`
-                  : `${style.link}`
+                  ? `${style.link} ${style.nharrow}`
+                  : `${style.link} `
               }
               href="/managercard"
               scroll={false}
@@ -74,8 +176,8 @@ const Nav = (props) => {
             </Link>
           </li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
