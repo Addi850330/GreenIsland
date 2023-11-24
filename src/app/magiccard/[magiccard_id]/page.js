0435654d1bfaeaf55e9703250126717689673540
Shiuan <berserker850330@gmail.com>
2023-11-24 17:50:30 +0800

    mobile card info page finish

--- src/app/magiccard/[magiccard_id]/page.js
+++ src/app/magiccard/[magiccard_id]/page.js
@@ -1,10 +1,63 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { data } from "../../data";
 import style from "../../page.module.css";
 
 const Post = ({ params }) => {
   const magiccard_id = params.magiccard_id;
 
-  return <div className={style.card}>post{magiccard_id}</div>;
+  const [cardname, setCardname] = useState("");
+  const [cardrank, setCardrank] = useState("");
+  const [cardnumber, setCardnumber] = useState("");
+  const [carddescription, setCarddescription] = useState("");
+  const [cardimg, setCardimg] = useState("");
+  const [cardimgorigin, setCardimgorigin] = useState("");
+
+  const cardid = magiccard_id;
+  const datas = data[1].magiccard;
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    datas.forEach((obj) => {
+      if (obj.id == cardid) {
+        setCardname(obj.name);
+        setCardrank(obj.rank);
+        setCardnumber(obj.number);
+        setCarddescription(obj.description);
+        setCardimg(obj.img);
+        setCardimgorigin(obj.imgorigin);
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div className={style.mcard}>
+        <div className={style.mcardimg}>
+          <img src={`.${cardimg}`} alt={cardname} />
+        </div>
+        <div className={style.mcardinfo}>
+          <div className={style.mcardnum}>Number : {cardnumber}</div>
+          <div className={style.mcardrank}>Rank : {cardrank}</div>
+          <div className={style.mcardname}>Name : {cardname}</div>
+          <div className={style.mcarddes}>
+            Information :
+            <br />
+            {carddescription}
+          </div>
+          <div className={style.downloadcard}>
+            <Link href={`.${cardimgorigin}`} className={style.mcardlink}>
+              Dowload
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Post;
