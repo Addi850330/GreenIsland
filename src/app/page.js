316fdffcdaf1c4f79f42e99bbb137a88f9c771c7
Shiuan <berserker850330@gmail.com>
2023-11-25 22:00:13 +0800

    home page animetion finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import style from "./page.module.css";
+
 export default function Home() {
   let [sec, setSec] = useState(0);
 
@@ -9,7 +10,6 @@ export default function Home() {
       return preCount + 1;
     });
   };
-
   useEffect(
     function () {
       // console.log("update");
@@ -23,26 +23,44 @@ export default function Home() {
   if (sec === 3) {
     window.location.href = "/specifycard";
   }
+
   return (
-    <div className={style.pageout}>
-      <div className={style.loadpage}>
-        <div className={style.loadimg}>
-          <img className={style.imgfront} src="./img/cardback.png" alt="" />
-          <img className={style.imgback} src="./img/cardback.png" alt="" />
-        </div>
-        <div className={style.loadtext}>
-          <p>L</p>
-          <p>o</p>
-          <p>a</p>
-          <p>d</p>
-          <p>i</p>
-          <p>n</p>
-          <p>g</p>
-          <p>.</p>
-          <p>.</p>
-          <p>.</p>
+    <>
+      <div className={style.pageout}>
+        <div className={style.loadpage}>
+          <div className={style.loadimg}>
+            <img className={style.imgfront} src="./img/cardback.png" alt="" />
+            <img className={style.imgback} src="./img/cardback.png" alt="" />
+          </div>
+          <div className={style.loadtext}>
+            <p>L</p>
+            <p>o</p>
+            <p>a</p>
+            <p>d</p>
+            <p>i</p>
+            <p>n</p>
+            <p>g</p>
+            <p>.</p>
+            <p>.</p>
+            <p>.</p>
+          </div>
+          <div data-aos="fade-left" className={style.loadanimel}>
+            <img src="./img/homepage/killa.png" alt="" />
+          </div>
+          <div data-aos="fade-down-right" className={style.loadanimelb}>
+            <img src="./img/homepage/killab.png" alt="" />
+          </div>
+          <div data-aos="flip-up" className={style.loadanimem}>
+            <img src="./img/homepage/hero.png" alt="" />
+          </div>
+          <div data-aos="fade-right" className={style.loadanimer}>
+            <img src="./img/homepage/gon.png" alt="" />
+          </div>
+          <div data-aos="fade-up-left" className={style.loadanimerb}>
+            <img src="./img/homepage/gonb.png" alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
